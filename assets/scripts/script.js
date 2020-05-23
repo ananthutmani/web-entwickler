f13eb29b0fe2e8b8af6ae69236802ec45a64711a
Ananthu T Mani <ananthum1999@gmail.com>
2020-05-23 21:59:19 +0530

    java

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,32 +1,36 @@
-// const head1 = document.querySelector("#h11");
-// const head2 = document.querySelector("#h22");
-var loader = document.getElementById("loader");
+const head1 = document.querySelector('#h11');
+const head2 = document.querySelector('#h22');
+const head3 = document.querySelector('#h33');
+var loader = document.getElementById('loader');
 // var navba = document.getElementById("navbar");
-window.addEventListener("load", function () {
-    loader.style.height = "100px";
-    loader.style.width = "100px";
-    loader.style.borderRadius = "50%";
-    loader.style.visibility = "hidden";
-    // navba.classList.remove("d-none");
-    // head1.classList.remove('d-none');
-    // head1.classList.add('animated', 'fadeInUp', 'delay-1s');
-    // head1.addEventListener('animationend', function () {
-    //     head2.classList.remove('d-none');
-    //     head2.classList.add('animated', 'fadeInUp');
-    // });
-});
-new WOW().init();
-// Navbar collapsing on outside touch
-// $(document).click(function (e) {
-//     if (!$(e.target).is('#navbar')) {
-//         $('.collapse').collapse('hide');
-//     }
-// });
+window.addEventListener('load', function () {
+	loader.style.height = '100px';
+	loader.style.width = '100px';
+	loader.style.borderRadius = '50%';
+	loader.style.visibility = 'hidden';
+	// navba.classList.remove("d-none");
+	head1.classList.remove('d-none');
+	head1.classList.add('animated', 'fadeIn');
+	head1.addEventListener('animationend', function () {
+		head2.classList.remove('d-none');
+		head2.classList.add('animated', 'fadeInLeft');
+		head3.classList.remove('d-none');
+		head3.classList.add('animated', 'fadeInDown');
 
-// // Navbar colour change on scrolling
-// $(function () {
-//     $(document).scroll(function () {
-//         var $nav = $("#navbr");
-//         $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
-//     });
-// });
+		// });
+	});
+	new WOW().init();
+	// Navbar collapsing on outside touch
+	// $(document).click(function (e) {
+	//     if (!$(e.target).is('#navbar')) {
+	//         $('.collapse').collapse('hide');
+	//     }
+	// });
+
+	// // Navbar colour change on scrolling
+	// $(function () {
+	//     $(document).scroll(function () {
+	//         var $nav = $("#navbr");
+	//         $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+	//     });
+});
